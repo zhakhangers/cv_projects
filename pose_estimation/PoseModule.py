bccf683b92d6f3f10bbe79e9ce92526fa700bd99
zhakhangers <73777397+zhakhangers@users.noreply.github.com>
2021-07-23 22:32:36 +0600

    Add files via upload

---
+++ pose_estimation/PoseModule.py
@@ -0,0 +1,71 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class poseDetector():
+
+    def __init__(self, mode = False, upBody = False, smooth = True, detectionCon = 0.5, trackCon = 0.5):
+
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+
+    def findPose(self, img, draw = True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+
+        return img
+
+    def findPosition (self, img, draw=True):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                # print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 6, (255, 0, 200), cv2.FILLED)
+
+        return lmList
+
+
+def main():
+
+    cap = cv2.VideoCapture('PoseVideos/1.mp4')
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            print(lmList[8])
+            cv2.circle(img, (lmList[8][1], lmList[8][2]), 16, (255, 10, 1), cv2.FILLED)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+
+
+if __name__ == "__main__":
+    main()
