0f7e71c2f6e009fe6f85474516370e77fdf00911
zhakhangers <73777397+zhakhangers@users.noreply.github.com>
2021-08-13 19:36:16 +0600

    Add files via upload

---
+++ cv_app/Scripts/PageNavigation.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PageNavigation : MonoBehaviour
+{
+
+    public void StartUpPage()
+    {
+        SceneManager.LoadScene("StartUpPage");
+    }
+
+    public void HomePage()
+    {
+        SceneManager.LoadScene("HomePage");
+    }
+
+    public void Chapter1()
+    {
+        SceneManager.LoadScene("Chapter1");
+    }
+
+    public void Chapter2()
+    {
+        SceneManager.LoadScene("Chapter2");
+    }
+
+    public void Chapter3()
+    {
+        SceneManager.LoadScene("Chapter3");
+    }
+
+    public void Chapter4()
+    {
+        SceneManager.LoadScene("Chapter4");
+    }
+
+    public void Chapter5()
+    {
+        SceneManager.LoadScene("Chapter5");
+    }
+
+    public void Chapter6()
+    {
+        SceneManager.LoadScene("Chapter6");
+    }
+
+    public void Chapter7()
+    {
+        SceneManager.LoadScene("Chapter7");
+    }
+
+
+    public void WebcamTemplate()
+    {
+        SceneManager.LoadScene("WebcamTemplate");
+    }
+
+    public void FaceDetection()
+    {
+        SceneManager.LoadScene("FaceDetection");
+    }
+
+    public void ColorPicker()
+    {
+        SceneManager.LoadScene("ColorPicker");
+    }
+
+    public void DocumentScanner()
+    {
+        SceneManager.LoadScene("DocumentScanner");
+    }
+
+    public void QrReader()
+    {
+        SceneManager.LoadScene("QrReader");
+    }
+
+    public void ObjectDetectionMNSSD()
+    {
+        SceneManager.LoadScene("ObjectDetectionMNSSD");
+    }
+
+    public void ObjectDetectionYoloV4()
+    {
+        SceneManager.LoadScene("ObjectDetectionYoloV4");
+    }
+    public void MarkerLessAR()
+    {
+        SceneManager.LoadScene("MarkerLessAR");
+    }
+
+
+}
